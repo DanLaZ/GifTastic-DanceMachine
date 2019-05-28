0d30c89a2964caf4cf78ed6d331883c6053c3adb
DanLaZ <lazenberry_daniel@yahoo.com>
2019-05-28 13:18:14 -0400

    buttons append to the page

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -5,12 +5,39 @@ var danceMoves = ["The Hustle", "Dab", "The Cat Daddy", "The Floss", "The Robot"
 
 console.log(danceMoves.length);
 
+function alertDanceName() {
+
+    var danceName = $(this).attr("data-name");
+
+    alert(danceName);
+}
+
 /* queryURL for Giphy API */
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=g0AzH16XHsMCcQY7krnPL8aac12JbfXJ";
 
 $.ajax({
+
   url: queryURL,
   method: "GET"
 }).then(function(response) {
+
   console.log(response);
 });
+
+/* Create Buttons */
+
+function loadButtons(){
+
+    $("#buttons").empty();
+    for (var i = 0; i < danceMoves.length; i++) {
+        var danceButtons = $("<button>");
+        danceButtons.addClass("dance");
+        danceButtons.attr("data-name", danceMoves[i]);
+        danceButtons.text(danceMoves[i]);
+        $("#buttons").append(danceButtons);
+    }
+}
+
+$(document).on("click", ".dance", alertDanceName);
+
+loadButtons();
