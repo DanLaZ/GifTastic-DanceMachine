bbdf9c80947706ebe3bc7109ace017d65339e713
DanLaZ <lazenberry_daniel@yahoo.com>
2019-05-29 15:52:15 -0400

    gifs upload to the page | form that adds new buttons and styling follows

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,28 +1,60 @@
 
 /* Variables */
 
-var danceMoves = ["The Hustle", "Dab", "The Cat Daddy", "The Floss", "The Robot", "The Electric Slide", "The Stomp", "The Milly Rock", "The Running Man", "The MC Hammer", "The Shuffle", "Hit Dem Folk", "Shoot", "Vogue", "The Moonwalk", "The Dougie", "The Twist", "The Carlton", "Sangria Wine"];
+var danceMoves = ["The Hustle", "Dab", "The Cat Daddy", "The Floss", "The Robot", "The Electric Slide", "The Stomp", "The Milly Rock", "The Running Man", "The MC Hammer", "The Shuffle", "Hit Dem Folk", "Shoot", "Vogue", "The Moonwalk", "The Dougie", "The Twist", "The Carlton", "Sangria Wine", "Daggering", "Dutty Wine", "The Harlem Shake", "The Macarena"];
 
 console.log(danceMoves.length);
 
-function alertDanceName() {
+// function alertDanceName() {
 
-    var danceName = $(this).attr("data-name");
+//     var danceName = $(this).attr("data-name");
 
-    alert(danceName);
-}
+//     alert(danceName);
+// }
 
 /* queryURL for Giphy API */
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=g0AzH16XHsMCcQY7krnPL8aac12JbfXJ";
+// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=g0AzH16XHsMCcQY7krnPL8aac12JbfXJ";
 
-$.ajax({
+function displayGif(){
+    
+    var dance = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dance + "+" + "dance" + "&api_key=g0AzH16XHsMCcQY7krnPL8aac12JbfXJ&limit=10";
 
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        
+        var dataArrayNum = response.data;
+        console.log(dataArrayNum);
 
-  console.log(response);
-});
+        for (var i = 0; i < dataArrayNum.length; i++) {
+            
+
+            
+            $("<img>").addClass("gif");
+            $("<img>").attr("src", dataArrayNum[i].images.downsized_large.url);
+
+            $("#gifDump").append($("<p>").text(dance));
+
+            $("#gifDump").append($("<p>").text("Rating: " + dataArrayNum[i].rating.toUpperCase()));
+
+            $("#gifDump").append($("<img>").attr("src", dataArrayNum[i].images.downsized_large.url));
+
+        }
+
+        
+    
+
+
+
+
+
+    });
+
+
+
+}
 
 /* Create Buttons */
 
@@ -38,6 +70,6 @@ function loadButtons(){
     }
 }
 
-$(document).on("click", ".dance", alertDanceName);
+$(document).on("click", ".dance", displayGif);
 
 loadButtons();
